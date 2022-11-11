619f26466243a430c57761c8f9d551315fbf61ca
JeyunSong <jyunart@naver.com>
2022-11-11 17:01:52 +0900

    [Update] MyPageService TestCode_2

--- Shop/src/test/java/product/service/mypage/MyPageServiceTest.java
+++ Shop/src/test/java/product/service/mypage/MyPageServiceTest.java
@@ -1,20 +1,24 @@
 package product.service.mypage;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import product.dto.mypage.MyPageResponseDto;
 import product.entity.product.Category;
+import product.entity.product.Order;
 import product.entity.product.Product;
 import product.entity.product.ProductInfo;
 import product.entity.user.Authority;
 import product.entity.user.User;
+import product.exception.ExceptionType;
+import product.exception.RequestException;
 import product.repository.product.CategoryRepository;
 import product.repository.product.OrderRepository;
 import product.repository.product.ProductInfoRepository;
@@ -22,7 +26,12 @@ import product.repository.product.ProductRepository;
 import product.repository.user.UserRepository;
 import product.service.product.TestConfig;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static product.exception.ExceptionType.ACCESS_DENIED_EXCEPTION;
+import static product.exception.ExceptionType.ORDER_FINISH_EXCEPTION;
 
 @DataJpaTest
 @Transactional
@@ -89,10 +98,61 @@ class MyPageServiceTest {
     }
 
     @Test
+    @DisplayName("주문 조회")
     void myPage() {
+        // GIVEN
+        Pageable pageable = Pageable.ofSize(10);
+        Long orderNum = 3L; // 주문 수량 : 3
+
+        Product product = productRepository.findByProductId(1L); // Stock : 10
+        User user = userRepository.findByUserId(1L);
+
+        product.getProductInfo().order(orderNum); // 주문 메소드
+
+        Order order = new Order(product, orderNum, user);
+        orderRepository.save(order);  // 주문 저장
+
+        // WHEN
+        Page<MyPageResponseDto> list = orderRepository.orderFilter(user,pageable);
+
+        // THEN
+        assertThat(list.getTotalElements()).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("주문 취소")
     void cancel() {
+        // GIVEN
+        Pageable pageable = Pageable.ofSize(10);
+        Long orderNum = 3L; // 주문 수량 : 3
+
+        Product product = productRepository.findByProductId(1L); // 재고 : 10
+        User user = userRepository.findByUserId(1L);
+
+        product.getProductInfo().order(orderNum); // 주문 메소드  // 재고 : 7, 상태 : 배송중
+        Order order = new Order(product, orderNum, user); // 주문 저장
+
+        Product productCheck = productRepository.findByProductId(order.getProduct().getProductId()); // 주문된 상품 정보
+
+        // WHEN
+        if (!user.equals(order.getUser()))   // 주문자 일치여부 판단
+            throw new RequestException(ACCESS_DENIED_EXCEPTION);
+
+
+        if (order.getOrderStatus().equals("배송완료"))  // 배송완료 시 취소 불가
+            throw new RequestException(ORDER_FINISH_EXCEPTION);
+
+
+        if (productCheck == null) // 주문이 제대로 됐는지 확인
+            throw new RequestException(ExceptionType.NOT_FOUND_EXCEPTION);
+
+        // -> 위 3개의 예외처리가 잘 실행되는지 테스트코드 작성해야합니다.
+
+        product.getProductInfo().cancel(order.getOrderNum()); // 재고 상태 변경
+        order.cancel(); // 진행 상황 변경
+
+        // THEN
+        assertThat(product).isEqualTo(productCheck);
+        assertThat(product.getProductInfo().getStock()).isEqualTo(10);
     }
 }
