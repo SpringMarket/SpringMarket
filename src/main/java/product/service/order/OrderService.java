6442034eb8a576f23f104edffb37a8cb8de037f0
Suyoung Yun <8904su@gmail.com>
2022-12-07 16:21:08 +0900

    [Create] Thread limit

--- src/main/java/product/service/order/OrderService.java
+++ src/main/java/product/service/order/OrderService.java
@@ -50,7 +50,7 @@ public class OrderService {
         // 상품 정보 변경
         product.getProductInfo().plusPreference(orderNum, user.getAge());
 
-        productRepository.save(product);
+//        productRepository.saveAndFlush(product);
 
         Orders order = new Orders(orderNum, "배송중", product, user);
         // 주문 데이터 저장
